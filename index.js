b19a6e961d83cd5bc4b18df57ffc419acaf8b59e
intrnl <intrnl28+git@gmail.com>
2019-11-09 00:51:21 +0700

    ESLint

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { Plugin } = require('powercord/entities')
-const { React, getModuleByDisplayName } = require('powercord/webpack')
-const { inject, uninject } = require('powercord/injector')
-const { findInReactTree } = require('powercord/util')
+const { Plugin } = require('powercord/entities');
+const { React, getModuleByDisplayName } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
 
-const SendBtn = require('./components/SendBtn')
+const SendBtn = require('./components/SendBtn');
+
 
 const event = new KeyboardEvent('keypress', {
   key: 'Enter',
@@ -11,42 +12,42 @@ const event = new KeyboardEvent('keypress', {
   which: 13,
   keyCode: 13,
   bubbles: true,
-})
+});
 
 
 class SendButton extends Plugin {
   startPlugin () {
-    this._patchButton()
+    this._patchButton();
   }
 
   pluginWillUnload () {
-    uninject('powercord-send-button_cta')
+    uninject('powercord-send-button_cta');
   }
 
   async _patchButton () {
-    const ChannelTextArea = await getModuleByDisplayName('ChannelTextArea')
+    const ChannelTextArea = await getModuleByDisplayName('ChannelTextArea');
 
     inject('powercord-send-button_cta', ChannelTextArea.prototype, 'render', (_, res) => {
       const ButtonContainer = findInReactTree(res, (o) => (
         typeof o.className === 'string' && o.className.startsWith('buttons-')
-      ))
+      ));
 
       const textArea = findInReactTree(res, (o) => (
         typeof o.className === 'string' && o.className.startsWith('textArea-') &&
         o.onKeyPress
-      ))
+      ));
 
       ButtonContainer.children.push(
         React.createElement(SendBtn, {
           onClick: () => {
-            textArea.onKeyPress(event)
+            textArea.onKeyPress(event);
           },
         }),
-      )
+      );
 
-      return res
-    })
+      return res;
+    });
   }
 }
 
-module.exports = SendButton
+module.exports = SendButton;
